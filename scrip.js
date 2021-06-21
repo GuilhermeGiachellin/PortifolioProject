f13c6db2fb25d8fc63bdb4d520bd98e1efef0d78
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-06-21 17:58:22 -0300

    Lynter Correction

--- scrip.js
+++ scrip.js
@@ -1,58 +1,57 @@
 const Hamburguer = document.getElementById('hamburguer');
-const Display_menu = document.getElementById('display_menu');
-const Chm_btn = document.getElementById('chm_btn');
-const Ds_port = document.getElementById('ds_port');
-const Ds_abt = document.getElementById('ds_abt');
-const Ds_cnt = document.getElementById('ds_cnt');
+const DisplayMenu = document.getElementById('display_menu');
+const ChmBtn = document.getElementById('chm_btn');
+const DsPort = document.getElementById('ds_port');
+const DsAbt = document.getElementById('ds_abt');
+const DsCnt = document.getElementById('ds_cnt');
 const sec = [];
 
- for(let i = 1; i <= 5; i++) {
-   sec.push(document.getElementById(`sec_${i}`))
+for (let i = 1; i <= 5; i + 1) {
+  sec.push(document.getElementById(`sec_${i}`))
 }
 
-hamburguer.addEventListener('click', (Event) => {
+Hamburguer.addEventListener('click', (Event) => {
   display_menu.style = 'display: flex; animation-name: display_menu_animation; animation-duration: 0.5s;';
-    for(let i = 1; i < 5; i++){
+    for (let i = 0; i < 5; i + 1) {
       sec[i].style = 'filter: blur(3px); -webkit-filter: blur(3px);'
     }
 });
 
-Chm_btn.addEventListener('click', (Event) => {
+ChmBtn.addEventListener('click', (Event) => {
     Event.preventDefault();  
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1; i < 5; i++){
+    for (let i = 0; i < 5; i + 1) {
         sec[i].style = ''
     }
 });
 
-Ds_port.addEventListener('click', (Event) => {
+DsPort.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1; i < 5; i++){
+    for (let i = 0; i < 5; i + 1) {
         sec[i].style = ''
     }
 });
 
-Ds_abt.addEventListener('click', (Event) => {
+DsAbt.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1; i < 5; i++){
+    for (let i = 0; i < 5; i + 1) {
         sec[i].style = ''
     }
 });
 
-Ds_cnt.addEventListener('click', (Event) => {
+DsCnt.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1; i < 5; i++){
+    for (let i = 0; i < 5; i + 1) {
         sec[i].style = ''
-    }  
+    }
 });
-
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
