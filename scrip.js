acd8221f5333ef869ee137b2e04100baeca1c6be
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-06-21 17:03:39 -0300

    Linters correction

--- scrip.js
+++ scrip.js
@@ -1,54 +1,54 @@
-let hamburguer = document.getElementById("hamburguer");
-let display_menu = document.getElementById("display_menu");
-let chm_btn = document.getElementById("chm_btn");
-let ds_port = document.getElementById("ds_port");
-let ds_abt = document.getElementById("ds_abt");
-let ds_cnt = document.getElementById("ds_cnt");
-let sec = [];
+const Hamburguer = document.getElementById('hamburguer');
+const Display_menu = document.getElementById('display_menu');
+const Chm_btn = document.getElementById('chm_btn');
+const Ds_port = document.getElementById('ds_port');
+const Ds_abt = document.getElementById('ds_abt');
+const Ds_cnt = document.getElementById('ds_cnt');
+const sec = [];
 
-for(let i = 1;i <= 5; i++){
-    sec.push(document.getElementById(`sec_${i}`))
+ for(let i = 1; i <= 5; i++) {
+   sec.push(document.getElementById(`sec_${i}`))
 }
 
-hamburguer.addEventListener('click', (Event) =>{
-    display_menu.style = 'display: flex; animation-name: display_menu_animation; animation-duration: 0.5s;' 
-    for(let i = 1;i < 5; i++){
-        sec[i].style = 'filter: blur(3px); -webkit-filter: blur(3px);'
+hamburguer.addEventListener('click', (Event) => {
+  display_menu.style = 'display: flex; animation-name: display_menu_animation; animation-duration: 0.5s;';
+    for(let i = 1; i < 5; i++){
+      sec[i].style = 'filter: blur(3px); -webkit-filter: blur(3px);'
     }
 });
 
-chm_btn.addEventListener('click', (Event) => {
+Chm_btn.addEventListener('click', (Event) => {
     Event.preventDefault();  
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1;i < 5; i++){
+    for(let i = 1; i < 5; i++){
         sec[i].style = ''
     }
 });
 
-ds_port.addEventListener('click', (Event) => {
+Ds_port.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1;i < 5; i++){
+    for(let i = 1; i < 5; i++){
         sec[i].style = ''
     }
 });
 
-ds_abt.addEventListener('click', (Event) => {
+Ds_abt.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1;i < 5; i++){
+    for(let i = 1; i < 5; i++){
         sec[i].style = ''
     }
 });
 
-ds_cnt.addEventListener('click', (Event) => {
+Ds_cnt.addEventListener('click', (Event) => {
     Event.preventDefault();
     setTimeout(function(){display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'}, 0)
     setTimeout(function(){display_menu.style = 'display: none;'}, 500)
-    for(let i = 1;i < 5; i++){
+    for(let i = 1; i < 5; i++){
         sec[i].style = ''
     }  
 });
