0dc1bd4a0b9efa8e88189efdf8bd92c198484719
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-06-21 19:37:24 -0300

    Memory overflow
    -The i + 1 inside of the for is not working, so I used i++ again.

--- scrip.js
+++ scrip.js
@@ -1,54 +1,57 @@
-const Hamburguer = document.getElementById('hamburguer');
-const DisplayMenu = document.getElementById('display_menu');
-const ChmBtn = document.getElementById('chm_btn');
-const DsPort = document.getElementById('ds_port');
-const DsAbt = document.getElementById('ds_abt');
-const DsCnt = document.getElementById('ds_cnt');
-const sec = [];
+var Hamburguer = document.getElementById('hamburguer');
+var Display_menu = document.getElementById('display_menu');
+var Chmbtn = document.getElementById('chm_btn');
+var Dsport = document.getElementById('ds_port');
+var Dsabt = document.getElementById('ds_abt');
+var Dscnt = document.getElementById('ds_cnt');
+var sec = [];
 
-for (let i = 1; i <= 5; i + 1) {
+for (let i = 1; i < 6; i++) {
   sec.push(document.getElementById(`sec_${i}`));
 }
 
 Hamburguer.addEventListener('click', (Event) => {
   Event.preventDefault(); 
-  DisplayMenu.style = 'display: flex;animation-name: display_menu_animation;animation-duration: 0.5s;';
-  for (let i = 0; i < 5; i + 1) {
+  Display_menu.style = 'display: flex; animation-name: display_menu_animation; animation-duration: 0.5s;';
+  for (let i = 0; i < 5; i++) {
     sec[i].style = 'filter: blur(3px); -webkit-filter: blur(3px);';
-  }
+  }  
 });
-ChmBtn.addEventListener('click', (Event) => {
+
+Chmbtn.addEventListener('click', (Event) => {
   Event.preventDefault();  
-  setTimeout(() => { DisplayMenu.style = 'display: flex;animation-name: close_menu_animation;animation-duration: 0.5s;'; }, 0);
-  setTimeout(() => { DisplayMenu.style = 'display: none;'; }, 500);
-  for (let i = 0; i < 5; i + 1) {
+  setTimeout(function () { Display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;' }, 0);
+  setTimeout(function () { Display_menu.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i++) {
+    sec[i].style = '';
+  }  
+});
+
+console.log("This works1")
+Dsport.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  setTimeout(function () { Display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;' }, 0);
+  setTimeout(function () { Display_menu.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i++) {
+    sec[i].style = '';
+  }  
+});
+
+console.log("This works2")
+Dsabt.addEventListener('click', (Event) => {
+  Event.preventDefault();
+  setTimeout(function () { Display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;' }, 0);
+  setTimeout(function () { Display_menu.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i++) {
     sec[i].style = '';
   }
 });
-
-DsPort.addEventListener('click', (Event) => {
+  
+Dscnt.addEventListener('click', (Event) => {
   Event.preventDefault();
-  setTimeout(function () { DisplayMenu.style = 'display: flex;animation-name: close_menu_animation;animation-duration: 0.5s;'; }, 0);
-  setTimeout(function () { DisplayMenu.style = 'display: none;'; }, 500);
-  for (let i = 0; i < 5; i + 1) {
-    sec[i].style = '';
-  }
-});
-
-DsAbt.addEventListener('click', (Event) => {
-  Event.preventDefault();
-  setTimeout(function () { DisplayMenu.style = 'display: flex;animation-name: close_menu_animation;animation-duration: 0.5s;'; }, 0);
-  setTimeout(function () { DisplayMenu.style = 'display: none;'; }, 500);
-  for (let i = 0; i < 5; i + 1) {
-    sec[i].style = '';
-  }
-});
-
-DsCnt.addEventListener('click', (Event) => {
-  Event.preventDefault();
-  setTimeout(function () { DisplayMenu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;'; }, 0);
-  setTimeout(function () { DisplayMenu.style = 'display: none;'; }, 500);
-  for (let i = 0; i < 5; i + 1) {
+  setTimeout(function () { Display_menu.style = 'display: flex; animation-name: close_menu_animation; animation-duration: 0.5s;' }, 0);
+  setTimeout(function () { Display_menu.style = 'display: none;'; }, 500);
+  for (let i = 0; i < 5; i++) {
     sec[i].style = '';
   }
 });
@@ -57,7 +60,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth;'
+      behavior: 'smooth'
     });
   });
 });
